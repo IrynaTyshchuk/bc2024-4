6ac2588cce257433ef3c42331421b873dd58ef2f
IrynaTyshchuk <IRYNA.TYSHCHUK.FEI@lnu.edu.ua>
2024-10-23 20:47:31 +0300

    Chenged

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const http = require ('http');
-const { Command } = require('commander');
-const fs = require ('fs');
+const fs = require("fs");
+const { Command } = require("commander");
+const http = require("http");
 
 const program = new Command();
-
 program
-.option('-h, --host <host>', 'server address')
-.option('-p, --port <port>', 'server port')
-.option('-c, --cache <path>', 'path to cache');
+    .option("-h, --host <host>")
+    .option("-p, --port <port>")
+    .option("-c, --cache <path>");
+
 program.parse(process.argv);
-const { host,port,cache} = program.opts();
-if (!fs.existsSync(cache)){
-    console.error('cache directory doesn`t exist');
+const { host, port, cache } = program.opts();
+
+// Перевірка на існування директорії
+if (!fs.existsSync(cache)) {
+    console.error("Cache directory does not exist");
     process.exit(1);
 }
 
-const server = http.createServer((req,res) =>{
+// Створення сервера
+const server = http.createServer((req, res) => {
     res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello world\n");
 });
+
+// Запуск сервера
 server.listen(port, host, () => {
-    console.log('Server running');
+    console.log(`Server running at http://${host}:${port}/`);
 });
